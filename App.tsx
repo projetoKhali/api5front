bcf97cbffaf667d6cc4f685f690a792d2e3d8d1d
Incivius <marcos.santos232@fatec.sp.gov.br>
2024-09-11 19:27:27 -0300

    feat: setup react-native

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>API5 FRONT-END</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
