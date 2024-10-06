d50fab4b098c89ec2d204fb249012a00ee7527ff
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-10-06 11:23:53 -0300

    refactor: move `BarChart` styles into dedicated `StyleSheet` constant

--- src/components/barChart.tsx
+++ src/components/barChart.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { View, Text, Dimensions } from 'react-native';
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
 import { RFPercentage } from 'react-native-responsive-fontsize';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryLabel } from 'victory-native';
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryLabel,
+} from 'victory-native';
 
 type DataItem = {
   month: string;
@@ -16,10 +22,11 @@ export default function BarChart({ data }: BarChartProps) {
   const { width } = Dimensions.get('window');
 
   const labelAngle = width < 400 ? -45 : 0;
-  
   return (
-    <View style={{ backgroundColor: '#fff', paddingTop: RFPercentage(2), borderRadius: 10, alignItems: 'center', height: '100%', justifyContent: 'center', paddingBottom: RFPercentage(2) }}>
-      <Text style={{ fontSize: RFPercentage(1.5) }}> Tempo médio de contratação (Em dias)</Text>
+    <View style={styles.container}>
+      <Text style={styles.chartTitle}>
+        Tempo médio de contratação (Em dias)
+      </Text>
       <VictoryChart
         theme={VictoryTheme.material}
         domainPadding={{ x: RFPercentage(2), y: RFPercentage(1) }}
@@ -27,24 +34,24 @@ export default function BarChart({ data }: BarChartProps) {
       >
         <VictoryAxis
           style={{
-            axis: { stroke: "#756f6a" },
-            ticks: { stroke: "grey", size: 5 },
+            axis: { stroke: '#756f6a' },
+            ticks: { stroke: 'grey', size: 5 },
             tickLabels: {
               fontSize: RFPercentage(1.2),
               fill: '#333',
               angle: labelAngle,
               textAnchor: labelAngle === 0 ? 'middle' : 'end',
             },
-            grid: { stroke: "none" },
+            grid: { stroke: 'none' },
           }}
         />
 
         <VictoryAxis
           dependentAxis
           style={{
-            axis: { stroke: "none" },
-            tickLabels: { fill: "none" },
-            grid: { stroke: "none" },
+            axis: { stroke: 'none' },
+            tickLabels: { fill: 'none' },
+            grid: { stroke: 'none' },
           }}
         />
 
@@ -60,7 +67,7 @@ export default function BarChart({ data }: BarChartProps) {
           labelComponent={<VictoryLabel dy={-10} />}
           style={{
             data: {
-              fill: "#F28727",
+              fill: '#F28727',
               width: RFPercentage(2),
               borderRadius: 4,
             },
@@ -75,3 +82,16 @@ export default function BarChart({ data }: BarChartProps) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    paddingTop: RFPercentage(2),
+    borderRadius: 10,
+    alignItems: 'center',
+    height: '100%',
+    justifyContent: 'center',
+    paddingBottom: RFPercentage(2),
+  },
+  chartTitle: { fontSize: RFPercentage(1.5) },
+});
