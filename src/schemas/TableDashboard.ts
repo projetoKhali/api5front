11d6d7efc3c43703286a36929f83028e6c03c1a0
markusgomes <contato.markus@gmail.com>
2024-10-18 17:04:19 -0300

    fix: schema tableDashboard

--- src/schemas/TableDashboard.ts
+++ src/schemas/TableDashboard.ts
@@ -1,7 +1,12 @@
-interface TableRow {
+export interface TableRow { // Interface para as linhas da tabela, permitindo dados flexíveis.
     [key: string]: any;
 }
 
-export default interface TableRowResponse {
+export interface TableRequest { // Interface genérica para requisições com filtros dinâmicos.
+    [key: string]: any;
+}
+
+export default interface TableRowResponse { // Interface para a resposta contendo os dados da tabela.
     tableData: TableRow[];
 }
+
