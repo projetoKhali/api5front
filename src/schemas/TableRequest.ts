a27f46a65d4f44cc05a3aa7ec8964cf2278ffd26
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-10-20 23:14:20 -0300

    style: apply code style to varied files

--- src/schemas/TableRequest.ts
+++ src/schemas/TableRequest.ts
@@ -1,12 +1,11 @@
-
 export interface TableRequest {
-    recruiters: number[];
-    processes: number[];
-    vacancies: number[];
-    dateRange: {
-        startDate: string;
-        endDate: string;
-    };
-    processStatus: number[];
-    vacancyStatus: number[];
+  recruiters: number[];
+  processes: number[];
+  vacancies: number[];
+  dateRange: {
+    startDate: string;
+    endDate: string;
+  };
+  processStatus: number[];
+  vacancyStatus: number[];
 }
